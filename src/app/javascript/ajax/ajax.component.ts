d870fdf5ea2379b93a96218e130a18e6cc46ccef
rkuhldude77 <rkuhldude77@gmail.com>
2017-03-21 17:00:07 -0600

    Added components for other assignments from Javascript. Needs code

--- src/app/javascript/ajax/ajax.component.ts
+++ src/app/javascript/ajax/ajax.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DomSanitizer, SafeUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'ajax',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AjaxComponent implements OnInit {
 
-  constructor() { }
+  private frameSite: string = "";
+  private trustedUrl: SafeUrl;
+
+  private sites: {} = [
+    {
+      "display": "Learn HTML",
+      "url": "http://www.w3schools.com/html/default.asp"
+    },
+    {
+      "display": "Learn CSS",
+      "url": "http://www.w3schools.com/css/default.asp"
+    },
+    {
+      "display": "Learn JavaScript",
+      "url": "http://www.w3schools.com/js/default.asp"
+    },
+    {
+      "display": "Learn JSON",
+      "url": "http://www.w3schools.com/json/default.asp"
+    },
+    {
+      "display": "Learn AJAX",
+      "url": "http://www.w3schools.com/ajax/default.asp"
+    }
+  ];
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.frameSite = "https://www.w3schools.com/default.asp";
+    this.trustedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.frameSite);
+  }
+
+  getUrl(id){
+    this.frameSite = id;
+    this.trustedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.frameSite);
+    console.log(this.frameSite);
   }
 
 }
